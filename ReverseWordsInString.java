a7d3e1fdeaf7dbdc8fecfdf9621657a10a1f7660
coding sigma <92455602+GaneshRenuse@users.noreply.github.com>
2025-01-22 16:23:16 +0530

    Create ReverseWordsInString.java

---
+++ ReverseWordsInString.java
@@ -0,0 +1,26 @@
+import java.util.Arrays.*;
+
+public class ReverseWordsInString
+{
+    public String ReverseWords(String s){
+        String[] words = s.split("\\s+");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            result.append(words[i]);
+            if (i != 0) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        ReverseWordsInString obj = new ReverseWordsInString();
+        String s = "the sky is blue";
+        String ans = obj.ReverseWords(s);
+
+        System.out.println(ans);
+    }
+}
