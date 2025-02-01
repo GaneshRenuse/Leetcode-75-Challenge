65a406e96fd857eb24a8372bb81b612db54595d7
coding sigma <92455602+GaneshRenuse@users.noreply.github.com>
2025-02-01 18:43:50 +0530

    Created file

---
+++ DeleteMiddleNode.java
@@ -0,0 +1,23 @@
+public class DeleteMiddleNode {
+
+    /*
+    public ListNode deleteMiddle(ListNode head) {
+        if (head == null || head.next == null) return null;
+
+        ListNode slow = head, fast = head, prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = slow.next;
+
+        return head;
+    }
+    */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ReversedLinkedList.java
@@ -0,0 +1,19 @@
+public class ReversedLinkedList {
+    /*
+        public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
+     */
+    public static void main(String[] args) {
+
+    }
+}
