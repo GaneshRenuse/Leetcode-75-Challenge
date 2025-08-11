0d67ea7a4201bc57e2ee547c20e5ec7ce53bca52
Ganesh Renuse <ganesh21renuse3@gmail.com>
2025-08-11 21:18:38 +0530

    Deleted older code files started re-solving the problems.

--- MergeStringsAlternately.java
+++ MergeStringsAlternately.java
@@ -1,9 +1,11 @@
-class Solution {
-    public String mergeAlternately(String word1, String word2) {
+import java.util.*;
+
+public class MergeStringsAlternately {
+    public static String mergeAlternately(String word1, String word2) {
         int length = Math.max(word1.length(), word2.length());
         StringBuilder merged = new StringBuilder();
 
-        for(int i = 0; i < length; i++){
+        for (int i = 0; i < length; i++) {
             if(i < word1.length()){
                 merged.append(word1.charAt(i));
             }
@@ -13,5 +15,16 @@ class Solution {
         }
 
         return merged.toString();
+    }
+    public static void main(String[] args){
+        //testcase1
+        String word1 = "abc";
+        String word2 = "pqr";
+        System.out.println(mergeAlternately(word1, word2));
+
+        //testcase2
+        word1 = "wxyz";
+        word2 = "rstuv";
+        System.out.println(mergeAlternately(word1, word2));
     }
 }
